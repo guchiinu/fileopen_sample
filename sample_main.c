2c2c0fac674ac4f1187487750aa345e8ff2a1b88
guchiinu <43141226+guchiinu@users.noreply.github.com>
2019-02-23 19:05:10 +0900

    新規作成
    新規作成

---
+++ sample_main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define FILE_BUFF_SIZE 10
+
+int main(void)
+{
+    char *p_filename = "sample.txt"; // ファイル名
+    FILE *p_file; // ファイル
+    char file_read_buffer[FILE_BUFF_SIZE]; // ファイル読み込み用バッファ
+
+    // ファイルを開く
+    p_file = fopen( p_filename, "r");
+    if (p_file == NULL)
+    {
+        // ファイルが開けない
+        printf("ファイルが開けない");
+        return -1;
+    }
+
+    // ファイルの中身を表示する
+    fread( file_read_buffer, FILE_BUFF_SIZE, 1, p_file);
+    printf("%s\n", file_read_buffer);
+
+    // ファイルを閉じる
+    fclose( p_file );
+    p_file = NULL;
+
+    return 0;
+}
